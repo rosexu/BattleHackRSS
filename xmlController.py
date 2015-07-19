54bab49fa257f5916af0b66a69d240d3ed5f6137
Rose <rosexu1996@yahoo.com>
2015-07-19 12:05:18 -0400

    porttttt

--- xmlController.py
+++ xmlController.py
@@ -115,4 +115,5 @@ def addNewItem(fileName, title, link, description, pubDate, guid):
 # handleRequest("20565627", "make candy")
 
 if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
